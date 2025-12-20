5141e7b364c78428bc4d0a1619b5131775917c6a
Kartik Upadhyay <kartikupadhyay613@gmail.com>
2025-12-20 14:15:17 +0530

    local origin added

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://food-delivery-frontend-web-applicat-pi.vercel.app',
+  origin: ['https://food-delivery-frontend-web-applicat-pi.vercel.app', 'http://localhost:5173'],
   credentials: true,}
 ));
 
